b19925c1e53384133eb7d228c5ca82c3e7d11f3c
Egar Almeida <egaralmeida@users.noreply.github.com>
2022-01-25 03:04:12 -0300

    reload on click

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+let dataJson;
+
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) => generateName(data));
 
 function generateName(data) {
+  dataJson = data;
+
   document.getElementById("prefix").textContent =
     data.prefixes[Math.floor(Math.random() * data.prefixes.length)].text;
 
@@ -12,3 +16,9 @@ function generateName(data) {
   document.getElementById("suffix").textContent =
     data.suffixes[Math.floor(Math.random() * data.suffixes.length)].text;
 }
+
+$(document).ready(function () {
+  $(".item--em").click(function () {
+    generateName(dataJson);
+  });
+});
