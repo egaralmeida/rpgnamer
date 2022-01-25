f30df0c064e4e0be3b5332fdf5276f9c27a5890c
Egar Almeida <egaralmeida@users.noreply.github.com>
2022-01-25 01:15:51 -0300

    first prototype

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-fetch('./data.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => generateName(data));
+
+function generateName(data) {
+  document.getElementById("prefix").textContent =
+    data.prefixes[Math.floor(Math.random() * data.prefixes.length)].text;
+
+  document.getElementById("item").textContent =
+    data.items[Math.floor(Math.random() * data.items.length)].text;
+
+  document.getElementById("suffix").textContent =
+    data.suffixes[Math.floor(Math.random() * data.suffixes.length)].text;
+}
